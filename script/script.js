a6e3c37add51c89a2109987e148de763df59e9d9
Shelley Dogra <sdogra@Shelleys-MBP.attlocal.net>
2015-07-18 00:44:16 -0500

    style changes

--- script/script.js
+++ script/script.js
@@ -2,8 +2,16 @@
 function calculaterupees(frm) {
 	var calculatedValue = 0
 	var currencyInput = frm.dollarAmount.value
+	var outputString = ""
 
 	calculatedValue = currencyInput * 63.37
-	frm.rupeeAmount.value = calculatedValue.toFixed(2)
+	
+	frm.rupeeAmount.value = calculatedValue.toFixed(2) + ' ' + "Lakhs"
+	
+/*
+	outputString = calculatedValue.toFixed(2)
+	
+	frm.rupeeAmount.value = outputString.toString() + ' ' + "Lakhs"
+*/
 
 }
